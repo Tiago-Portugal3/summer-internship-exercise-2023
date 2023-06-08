e511e6d329a35c66800bbc7dd3914553344a848d
tiago <tiagodiasportugal@gmail.com>
2023-06-08 14:38:41 +0100

    Finished project

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,19 +1,70 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
-/**
- * Created by aamado on 05-05-2023.
- */
 class SnailShellPattern implements ISnailShellPattern {
 
   /**
    * Method to get snailshell pattern
    * 
    * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
+   * @return order array of values that represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+
+    if (matrix.length == 0) {
+      return CompletableFuture.completedFuture(new int[0]);
+    }
+
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+    int[] snailShellArray = new int[rows * cols];
+    int index = 0;
+
+    // Test if the matrix is N*N
+    if (rows != cols) {
+      System.out.println("\nMatrix must be N*N\n");
+      return CompletableFuture.completedFuture(null);
+    }
+
+    int topRow = 0;
+    int bottomRow = rows - 1;
+    int leftCol = 0;
+    int rightCol = cols - 1;
+
+    //Llgorithm for the Snailshellpattern with time complexity O(N)
+    while (topRow <= bottomRow && leftCol <= rightCol) {
+      // Traverse the top row of the matrix from left to right
+      for (int i = leftCol; i <= rightCol; i++) {
+        snailShellArray[index++] = matrix[topRow][i];
+      }
+      topRow++;
+
+      // Traverse the right column of the matrix from top to bottom
+      for (int i = topRow; i <= bottomRow; i++) {
+        snailShellArray[index++] = matrix[i][rightCol];
+      }
+      rightCol--;
+
+      // Check if there are still rows and columns to traverse
+      if (topRow <= bottomRow) {
+        // Traverse the bottom row of the matrix from right to left
+        for (int i = rightCol; i >= leftCol; i--) {
+          snailShellArray[index++] = matrix[bottomRow][i];
+        }
+        bottomRow--;
+      }
+
+      if (leftCol <= rightCol) {
+        // Traverse the left column of the matrix from bottom to top
+        for (int i = bottomRow; i >= topRow; i--) {
+          snailShellArray[index++] = matrix[i][leftCol];
+        }
+        leftCol++;
+      }
+    }
+
+    return CompletableFuture.completedFuture(snailShellArray);
+  }
 }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.Arrays;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,5 +35,65 @@ public class SnailShellPatternTest {
     int[] result = count.get(10, TimeUnit.SECONDS);
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
+    System.out.println("\nTeste 1: " + Arrays.toString(result) + "\n");
   }
+
+  @Test
+  public void SnailShellPatternTest2()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    assertArrayEquals(result, expected);
+    System.out.println("\nTest 2 getSnailShell: " + Arrays.toString(result) + "\n");
+  }
+
+  @Test
+  public void SnailShellPatternTest7x7Matrix()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = {
+        { 1, 2, 3, 4, 5, 6, 7 },
+        { 24, 25, 26, 27, 28, 29, 8 },
+        { 23, 40, 41, 42, 43, 30, 9 },
+        { 22, 39, 48, 49, 44, 31, 10 },
+        { 21, 38, 47, 46, 45, 32, 11 },
+        { 20, 37, 36, 35, 34, 33, 12 },
+        { 19, 18, 17, 16, 15, 14, 13 }
+    };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = {
+        1, 2, 3, 4, 5, 6, 7,
+        8, 9, 10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19, 20, 21,
+        22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31, 32, 33, 34, 35,
+        36, 37, 38, 39, 40, 41, 42,
+        43, 44, 45, 46, 47, 48, 49
+    };
+    assertArrayEquals(result, expected);
+    System.out.println("\nTest 3 getSnailShell: " + Arrays.toString(result) + "\n");
+  }
+
+  @Test
+  public void testEmptyMatrix() throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = new int[0][0];
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = new int[0];
+    assertArrayEquals(result, expected);
+    System.out.println("\nTest for empty matrix: " + Arrays.toString(result) + "\n");
+  }
+
+  @Test
+  public void testNonSquareMatrix() throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = {
+        { 1, 2, 3 },
+        { 4, 5, 6 }
+    };
+    Future<int[]> result = new SnailShellPattern().getSnailShell(matrix);
+    assertNull(result.get(10, TimeUnit.SECONDS));
+  }
+
 }
